7dc5ab1b740cd4169dfecdda1863e1c20466aa11
cfddog <ivyleave@163.com>
2018-03-11 19:26:14 +0800

    modification of resconstruction continuing(1)

--- src/ReconstructSchemes.f90
+++ src/ReconstructSchemes.f90
@@ -231,9 +231,38 @@
     use ctrlpm
     type(BLOCK_TYPE),pointer :: pBlk
     integer no_blk
-    real    :: WENO_eps=1.e-6
+    real    :: rho_eps=0.05
     real    :: LM(5,5),RM(5,5),fvar(5,-3:3),hh(5)
     real    :: arr(nst-1:ned,3),brr(nst-1:ned),xrr(nst-1:ned) !solve a*x=b
+    real    :: h
+    i=icur
+    j=jcur
+    k=kcur
+    do i1=1,5
+       
+    do icnt=nst-1,ned
+       if(icnt .eq. nst-1) then
+           arr(icnt,1)=0.;arr(icnt,2)=1.;arr(icnt,3)=0.
+           brr(icnt)=25./12.*fp(i1,icnt+1)-23./12.*fp(i1,icnt+2)-13./12.*fp(i1,icnt+3)-1./4.*fp(i1,icnt+4)
+       elseif(icnt .eq. nst) then
+           arr(icnt,1)=0.;arr(icnt,2)=1.;arr(icnt,3)=0.
+           brr(icnt)=1./4.*fp(i1,icnt)+13./12.*fp(i1,icnt+1)-5./12.*fp(i1,icnt+2)+1./12.*fp(i1,icnt+3)
+       elseif(icnt .eq. ned) then
+           arr(icnt,1)=0.;arr(icnt,2)=1.;arr(icnt,3)=0.
+           brr(icnt)=25./12.*fp(i1,icnt)-23./12.*fp(i1,icnt-1)-13./12.*fp(i1,icnt-2)-1./4.*fp(i1,icnt-3)
+       else
+           arr(icnt,1)=3.;arr(icnt,2)=6.;arr(icnt,3)=1.
+           brr(icnt)=1./3.*fp(i1,icnt-1)+19./3.*fp(i1,icnt)+10./3.*fp(i1,icnt+1)
+       endif
+       !NND-2 subroutine if oscillation ocurred
+       beta_r=abs(rho(i+iinc,j+jinc,k+kinc)-rho(i,j,k))
+       if(beta_r .le. rho_eps) then
+          arr(icnt,1)=0.;arr(icnt,2)=1.;arr(icnt,3)=0.
+          dFp =fp(i1,icnt+1) - fp(i1,icnt  )
+          dFpn=fp(i1,icnt  ) - fp(i1,icnt-1)
+          brr(icnt)=fp(i1,icnt  )+0.5*vminmod(dFp,dFpn) 
+       endif
+    enddo
     
     return
     end subroutine
